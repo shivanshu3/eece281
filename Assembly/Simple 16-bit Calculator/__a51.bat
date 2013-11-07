c695bd697c44f2b1a5358ef8cfbad97cb1e51277
sgama <samsongama@gmail.com>
2013-11-06 20:48:48 -0800

    Uploaded Simple 16-bit calculator
    EECE 259: Lab 6

---
+++ Assembly/Simple 16-bit Calculator/__a51.bat
@@ -0,0 +1,5 @@
+@echo off
+D:
+cd "\User\Dropbox\Assembly\Lab 6\"
+"C:\CrossIDE\Call51\Bin\a51.exe" -l "D:\User\Dropbox\Assembly\Lab 6\Simple16bitcalc.asm"
+echo Crosside_Action Set_Hex_File D:\User\Dropbox\Assembly\Lab 6\Simple16bitcalc.HEX
