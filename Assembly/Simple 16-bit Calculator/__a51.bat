503acd11d2201ef0e074ac8861e2a60903c215ea
Shivanshu Goyal <shivanshu3@gmail.com>
2013-11-13 21:05:20 -0800

    removed Assembly folder

--- Assembly/Simple 16-bit Calculator/__a51.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-D:
-cd "\User\Dropbox\Assembly\Lab 6\"
-"C:\CrossIDE\Call51\Bin\a51.exe" -l "D:\User\Dropbox\Assembly\Lab 6\Simple16bitcalc.asm"
-echo Crosside_Action Set_Hex_File D:\User\Dropbox\Assembly\Lab 6\Simple16bitcalc.HEX
