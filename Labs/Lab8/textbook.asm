da3f630c89204ab4f1881aea98894eef2095ab6b
Shivanshu Goyal <shivanshu3@gmail.com>
2013-11-17 20:12:15 -0800

    7 segments work, zeros still show

--- Labs/Lab8/textbook.asm
+++ Labs/Lab8/textbook.asm
@@ -4,6 +4,113 @@ CSEG
 org 0000H
    ljmp MyProgram
 
+	CSEG                                        ; Absolute CODE segements at fixed memory locations
+    myLUT:                                        ; Look-up table for 7-seg displays
+    DB 0C0H, 0F9H, 0A4H, 0B0H, 099H                ; 0 TO 4
+    DB 092H, 082H, 0F8H, 080H, 090H                ; 4 TO 9
+
+CSEG    
+hex2bcd:
+        push acc
+        push psw
+        push AR0
+        
+        clr a
+        mov R3, a ; Initialize BCD to 00-00-00 
+        mov R4, a
+        mov R5, a
+        mov R6, a
+        mov b, #24  ; Loop counter.
+
+hex2bcd_L0:
+        ; Shift binary left        
+        mov a, R1
+        mov c, acc.7 ; This way x remains unchanged!
+        mov a, R0
+        rlc a
+        mov R0, a
+        mov a, R1
+        rlc a
+        mov R1, a
+        mov a, R2
+        rlc a
+        mov R2, a
+        
+    
+        ; Perform bcd + bcd + carry using BCD arithmetic
+        mov a, R3
+        addc a, R3
+        da a
+        mov R3, a
+        mov a, R4
+        addc a, R4
+        da a
+        mov R4, a
+        mov a, R5
+        addc a, R5
+        da a
+        mov R5, a
+        mov a, R6
+        addc a, R6
+        da a
+        mov R6, a
+      
+
+        djnz b, hex2bcd_L0
+
+        pop AR0
+        pop psw
+        pop acc
+        ret
+	
+Display:
+        mov dptr, #myLUT
+        ; Display Digit 0
+        mov A, R3
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX0, A
+        ; Display Digit 1
+        mov A, R3
+        swap a
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX1, A
+        ; Display Digit 2
+        mov A, R4
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX2, A
+        ; Display Digit 3
+        mov A, R4
+        swap a
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX3, A
+        ; Display Digit 4
+        mov A, R5
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX4, A
+        ; Display Digit 5
+        mov A, R5
+        swap a
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX5, A
+        ; Display Digit 6
+        mov A, R6
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX6, A
+        ; Display Digit 7
+        mov A, R6
+        swap a
+        anl a, #0fh
+        movc A, @A+dptr
+        mov HEX7, A
+        ret
+
 add24:
 	;x=x+y
 	mov a,R0
@@ -40,7 +147,7 @@ multiply24:
 ; On the DE2-8052, with a 33.33MHz clock, one cycle takes 30ns
 Wait1s:
 	mov R7,#0
-	mov R2, #178
+	mov R2, #176
 	L3: mov R1, #250
 	L2: mov R0, #250
 	L1: djnz R0, L1 ; 3 machine cycles-> 3*30ns*250=22.5us
@@ -107,6 +214,9 @@ forever:
 	mov LEDRB,R2
 	mov LEDRA,R1
 	mov LEDG,R0
+	
+	call hex2bcd
+	call display
 	
     jmp forever
 
